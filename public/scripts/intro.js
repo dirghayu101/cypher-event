26e28cf9fd282b8a189c918680cdcce62852684c
Cherub <tmajmer@gmail.com>
2023-02-18 11:54:08 +0530

    video section upload

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,5 +1,7 @@
 const video = document.getElementById("introVideo")
 const vd = document.getElementsByClassName("vd")
+const nextbtn = document.querySelectorAll(".gameStartBtn")
+const playBtn = document.querySelectorAll(".vd span")
 
 
 vd[0].addEventListener('click',()=>{
@@ -8,10 +10,17 @@ vd[0].addEventListener('click',()=>{
       }, 10000); 
  if(video.readyState===4){
     clearTimeout(timeout)
+    playBtn[0].style.display="none"
     video.play();
  }
 })
 
 video.addEventListener("ended",()=>{
+video.style.display="none";
+nextbtn[0].style.display="flex"
+    
+})
+nextbtn[0].addEventListener("click",()=>{
+
     window.location.href="http://localhost:5500/user/chatScreen"
 })
